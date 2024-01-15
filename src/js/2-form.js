c7eeda69781c9e0d3b036493a628d434780f9217
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-01-15 16:59:17 +0200

    20240115

---
+++ src/js/2-form.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('input', saveDataInLocal);
+let saveData = {};
+
+if(localStorage.getItem('feedback-form-state')) {
+  saveData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  for(let key in saveData) {
+    form.elements[key].value = saveData[key];
+  }
+}
+
+function saveDataInLocal({target: {value, name}}) {
+  saveData[name] = value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(saveData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+  if (email !== "" && message !== "") {
+    const userCredentials = {};
+    userCredentials.email = email;
+    userCredentials.message = message;
+    console.log(userCredentials);
+    localStorage.clear();
+    form.reset();
+  } 
+};
